fcde6af548c73a75fc9ae29d95052de35ce9d360
Haivex <demozial@gmail.com>
2021-04-20 12:04:42 +0200

    Added about content

---
+++ client/src/components/about/AboutContent.ts
@@ -0,0 +1,46 @@
+const project = {
+  name: "Project name",
+  description: "Project description",
+  gitHubLink:
+    "https://github.com/CodersCamp2020-HK/CodersCamp2020.Hackathon.Starter",
+  contributors: [
+    {
+      name: "Anna",
+      surname: "Marszałek",
+      description: "lorem ipsum dor si amet",
+      gitHubLink: "https://github.com/Ania-Em",
+    },
+    {
+      name: "Weronika",
+      surname: "Dziedzic",
+      description: "lorem ipsum dor si amet",
+      gitHubLink: "https://github.com/blackrabbit2",
+    },
+    {
+      name: "Tomasz",
+      surname: "Dudek",
+      description: "lorem ipsum dor si amet",
+      gitHubLink: "https://github.com/dudeek",
+    },
+    {
+      name: "Mateusz",
+      surname: "Baciak",
+      description: "lorem ipsum dor si amet",
+      gitHubLink: "https://github.com/bat098",
+    },
+    {
+      name: "Mateusz",
+      surname: "Król",
+      description: "lorem ipsum dor si amet",
+      gitHubLink: "https://github.com/KrolMateusz",
+    },
+    {
+      name: "Mateusz",
+      surname: "Kmieć",
+      description: "lorem ipsum dor si amet",
+      gitHubLink: "https://github.com/Haivex",
+    },
+  ],
+};
+
+export default project;

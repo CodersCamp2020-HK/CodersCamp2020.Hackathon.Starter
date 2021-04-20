1fb73abb78043ca87bcba4586458a5f56940c531
Haivex <demozial@gmail.com>
2021-04-20 18:16:31 +0200

    Positioning text

--- client/src/components/workInProgress/WorkInProgress.tsx
+++ client/src/components/workInProgress/WorkInProgress.tsx
@@ -1,20 +1,27 @@
-import { Grid, makeStyles, Typography } from "@material-ui/core";
+import { Grid, makeStyles, Typography, Theme } from "@material-ui/core";
 import React from "react";
-import UnderConstruction from './under-construction.png';
+import UnderConstruction from "./under-construction.png";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
   photo: {
-    height: "70vh",
+    width: "100vw",
   },
   text: {
+    marginTop: theme.spacing(1),
     textAlign: "center",
+    textTransform: "uppercase",
   },
-});
+}));
 const WorkInProgress = () => {
   const styles = useStyles();
   return (
     <Grid container direction="column" justify="center" alignItems="center">
-      <img className={styles.photo} title='Work in progress!' alt="Work in progress. From clipartmax under licence Personal Use: https://www.clipartmax.com/middle/m2i8N4G6m2K9d3d3_construction-png-free-download-work-in-progress-png/" src={UnderConstruction} />
+      <img
+        className={styles.photo}
+        title="Work in progress!"
+        alt="Work in progress. From clipartmax under licence Personal Use: https://www.clipartmax.com/middle/m2i8N4G6m2K9d3d3_construction-png-free-download-work-in-progress-png/"
+        src={UnderConstruction}
+      />
       <Typography className={styles.text} variant="h4">
         Strona w budowie! Zajrzyj później :)
       </Typography>

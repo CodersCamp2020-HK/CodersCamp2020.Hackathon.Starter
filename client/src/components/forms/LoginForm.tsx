e0bf2786501ed02c981fa324057564c4ac2a90ff
Mateusz Król <matek8952@gmail.com>
2021-04-21 10:51:17 +0200

    LoginForm inputs

--- client/src/components/forms/LoginForm.tsx
+++ client/src/components/forms/LoginForm.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import Button from '@material-ui/core/Button'
 import TextInput from '../inputs/textInput/TextInput'
+import { makeStyles } from '@material-ui/core'
 
 export interface FormInputs {
     email: string;
@@ -9,16 +10,27 @@ export interface FormInputs {
     repPassword: string;
 }
 
+const useStyles = makeStyles({
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
 const LoginForm = () => {
+    const classes = useStyles();
     const { control, handleSubmit } = useForm<FormInputs>();
     const onSubmit = (data: Record<string, any>) => {
         console.log(data)
     }
 
     return (
-        <form noValidate onSubmit={handleSubmit(onSubmit)}>
-            <TextInput control={control} name="email" />
-            <Button type="submit">Submit</Button>
+        <form noValidate onSubmit={handleSubmit(onSubmit)} className={classes.form}>
+            <TextInput control={control} name="email" label="Email" type="email" />
+            <TextInput control={control} name="password" label="Hasło" type="password" />
+            <Button type="submit" color="primary">Zaloguj się</Button>
         </form>
     )
 }
--- client/src/components/inputs/textInput/TextInput.tsx
+++ client/src/components/inputs/textInput/TextInput.tsx
@@ -6,6 +6,8 @@ import { FormInputs } from '../../forms/LoginForm';
 interface Props {
     name: string;
     control: Control<FormInputs>;
+    label: string;
+    type?: string;
 }
 
 // function isInObject<T extends { [key: string]: any }, K extends keyof T>(value: string, obj: T): value is K {
@@ -16,11 +18,12 @@ function isKeyOf<T>(value: string | number | symbol, obj: T): value is keyof T &
     return value in obj && typeof value === 'string';
 }
 
-const TextInput = ({ name, control }: Props) => {
-    if (!isKeyOf<FormInputs>(name, { email: '', repPassword: '', password: '' })) throw new Error('Tak być nie może!');
+const TextInput = ({ name, control, label, type }: Props) => {
+    if (!isKeyOf<FormInputs>(name, { email: '', repPassword: '', password: '' }))
+        throw new Error(`${name} does not exist in inputs interface for form`);
     const { field: { ref, ...inputProps } } = useController({ name, control, defaultValue: '' });
 
-    return <TextField inputRef={ref} {...inputProps} />
+    return <TextField variant="standard" label={label} type={type} inputRef={ref} {...inputProps} />
 }
 
 export default TextInput

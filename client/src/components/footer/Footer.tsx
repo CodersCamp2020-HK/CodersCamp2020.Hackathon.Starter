cadd8bb45ee54ce93e4266aa9f984aa76cf6da25
Haivex <demozial@gmail.com>
2021-04-21 14:50:52 +0200

    Added copyright and links

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,20 +1,44 @@
-import { makeStyles, Theme } from "@material-ui/core";
+import { Link, makeStyles, Theme, Typography } from "@material-ui/core";
+import { GitHub, YouTube } from "@material-ui/icons";
 
 export const FOOTER_HEIGHT = 48;
 
 const useStyles = makeStyles((theme: Theme) => ({
   footer: {
+    backgroundColor: theme.palette.primary.main,
     boxSizing: "border-box",
-    position: "absolute",
     bottom: 0,
-    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
     height: FOOTER_HEIGHT,
-    backgroundColor: theme.palette.primary.light,
+    position: "absolute",
+    width: "100%",
+  },
+  link: {
+    color: theme.palette.info.light,
+    marginRight: theme.spacing(2),
   },
 }));
 const Footer = () => {
   const styles = useStyles();
-  return <footer className={styles.footer}>Text</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <Typography color="textPrimary" variant="body2">
+          Copyright &copy; 2021 Hubert Kawałek Team. All right reserved
+        </Typography>
+      </div>
+      <div>
+        <Link className={styles.link} href="https://www.youtube.com">
+          <YouTube />
+        </Link>
+        <Link className={styles.link} href="https://www.github.com">
+          <GitHub />
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

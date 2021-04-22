a2cb002900bcc20e2eee8dd418607d0b217f0461
Mateusz Król <matek8952@gmail.com>
2021-04-22 16:22:15 +0200

    CheckboxGroup finished

--- client/src/components/forms/Example.tsx
+++ client/src/components/forms/Example.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { makeStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import RadioGroup from '../inputs/radioGroup/RadioGroup';
+import RadioGroup from '../inputs/radio/radioGroup/RadioGroup';
+import CheckboxGroup from '../inputs/checkboxGroup/CheckboxGroup';
 
 type ExampleInputs = {
     radioElo: string;
+    checbkoxYo: string[];
 };
 
 const radioOptions = [
@@ -24,6 +26,25 @@ const radioOptions = [
     {
         label: 'Elo4',
         value: 'elo4',
+    },
+];
+
+const checkboxOptions = [
+    {
+        label: 'Yo1',
+        value: 'yo1',
+    },
+    {
+        label: 'Yo2',
+        value: 'yo2',
+    },
+    {
+        label: 'Yo3',
+        value: 'yo3',
+    },
+    {
+        label: 'Yo4',
+        value: 'yo4',
     },
 ];
 
@@ -59,6 +80,13 @@ const Example = () => {
                 name='radioElo'
                 formLabel='wybierz elo'
                 color='primary'
+            />
+            <CheckboxGroup
+                control={control}
+                checkboxOptions={checkboxOptions}
+                name="checbkoxYo"
+                formLabel="Wybierz Yo"
+                color="primary"
             />
             <Button type='submit' color='primary'>
                 Klik

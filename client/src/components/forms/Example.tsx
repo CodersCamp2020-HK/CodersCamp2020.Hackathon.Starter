e5ac3d2f4d8231a3194460d0e3c9b2f4c449bb8b
Mateusz Król <matek8952@gmail.com>
2021-04-22 18:41:46 +0200

    Select finished

--- client/src/components/forms/Example.tsx
+++ client/src/components/forms/Example.tsx
@@ -1,67 +1,90 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { makeStyles } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import RadioGroup from '../inputs/radio/radioGroup/RadioGroup';
-import CheckboxGroup from '../inputs/checkboxGroup/CheckboxGroup';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { makeStyles } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import RadioGroup from "../inputs/radio/radioGroup/RadioGroup";
+import CheckboxGroup from "../inputs/checkboxGroup/CheckboxGroup";
+import Select from "../inputs/select/Select";
 
 type ExampleInputs = {
     radioElo: string;
     checbkoxYo: string[];
+    selectCze: string;
 };
 
 const radioOptions = [
     {
-        label: 'Elo1',
-        value: 'elo1',
+        label: "Elo1",
+        value: "elo1",
     },
     {
-        label: 'Elo2',
-        value: 'elo2',
+        label: "Elo2",
+        value: "elo2",
     },
     {
-        label: 'Elo3',
-        value: 'elo3',
+        label: "Elo3",
+        value: "elo3",
     },
     {
-        label: 'Elo4',
-        value: 'elo4',
+        label: "Elo4",
+        value: "elo4",
     },
 ];
 
 const checkboxOptions = [
     {
-        label: 'Yo1',
-        value: 'yo1',
+        label: "Yo1",
+        value: "yo1",
     },
     {
-        label: 'Yo2',
-        value: 'yo2',
+        label: "Yo2",
+        value: "yo2",
     },
     {
-        label: 'Yo3',
-        value: 'yo3',
+        label: "Yo3",
+        value: "yo3",
     },
     {
-        label: 'Yo4',
-        value: 'yo4',
+        label: "Yo4",
+        value: "yo4",
+    },
+];
+
+const selectOptions = [
+    {
+        label: "Cze1",
+        value: "cze1",
+    },
+    {
+        label: "Cze2",
+        value: "cze2",
+    },
+    {
+        label: "Cze3",
+        value: "cze3",
+    },
+    {
+        label: "Cze4",
+        value: "cze4",
     },
 ];
 
 const useStyles = makeStyles({
     form: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        padding: '2rem 5rem',
-        boxSizing: 'border-box',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        padding: "2rem 5rem",
+        boxSizing: "border-box",
     },
 });
 
 const Example = () => {
-    const { handleSubmit, control } = useForm<ExampleInputs>();
+    const { handleSubmit, control } = useForm<ExampleInputs>({
+        defaultValues: { selectCze: "cze1" },
+    });
     const classes = useStyles();
 
     const onSubmit = (data: ExampleInputs) => {
@@ -77,9 +100,9 @@ const Example = () => {
             <RadioGroup
                 control={control}
                 radioOptions={radioOptions}
-                name='radioElo'
-                formLabel='wybierz elo'
-                color='primary'
+                name="radioElo"
+                formLabel="wybierz elo"
+                color="primary"
             />
             <CheckboxGroup
                 control={control}
@@ -88,7 +111,13 @@ const Example = () => {
                 formLabel="Wybierz Yo"
                 color="primary"
             />
-            <Button type='submit' color='primary'>
+            <Select
+                control={control}
+                name="selectCze"
+                label="Cześć"
+                selectOptions={selectOptions}
+            />
+            <Button type="submit" color="primary">
                 Klik
             </Button>
         </form>

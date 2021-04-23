4481d26058c6d1947a2a8793ccde5046d89a5f2d
bat098 <mateusz.baciak@gmail.com>
2021-04-24 01:39:50 +0200

    actions buttons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
-import { Toolbar } from "@material-ui/core";
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Navbar from "./components/navbar/Navbar";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/core/styles";
-import { RestfulProvider } from "restful-react";
-import { ThemeProvider } from "@material-ui/core/styles";
-import { DarkTheme } from "./themes/DarkTheme";
-import { LightTheme } from "./themes/LightTheme";
+import { Toolbar } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import { RestfulProvider } from 'restful-react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { DarkTheme } from './themes/DarkTheme';
+import { LightTheme } from './themes/LightTheme';
 
-const isProductionEnv = process.env.NODE_ENV === "production";
-const devApiUrl = "http://localhost:8000";
+const isProductionEnv = process.env.NODE_ENV === 'production';
+const devApiUrl = 'http://localhost:8000';
 const baseApiUrl = isProductionEnv
   ? process.env.REACT_APP_PRODUCTION_API_URL ?? devApiUrl
   : devApiUrl;
@@ -24,23 +24,23 @@ interface IAppContext {
 
 export const AppContext = React.createContext<IAppContext>(null!);
 
-const StorageThemeKey = "darkTheme";
+const StorageThemeKey = 'darkTheme';
 
 function App() {
   useEffect(() => {
-    const domain = "meet.jit.si";
+    const domain = 'meet.jit.si';
     const options = {
-      roomName: "PickAnAppropriateMeetingNameHere",
+      roomName: 'PickAnAppropriateMeetingNameHere',
       width: 700,
       height: 700,
-      parentNode: document.querySelector("#meet"),
+      parentNode: document.querySelector('#meet'),
     };
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const api = new JitsiMeetExternalAPI(domain, options);
   }, []);
 
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
   const [darkTheme, setDarkTheme] = useState<boolean>(() => {
     return localStorage.getItem(StorageThemeKey) ? true : false;
   });
@@ -50,7 +50,7 @@ function App() {
     if (darkTheme) {
       localStorage.removeItem(StorageThemeKey);
     } else {
-      localStorage.setItem(StorageThemeKey, "1");
+      localStorage.setItem(StorageThemeKey, '1');
     }
     setDarkTheme(!darkTheme);
   };
@@ -63,13 +63,11 @@ function App() {
     <RestfulProvider base={baseApiUrl}>
       <ThemeProvider theme={darkTheme ? DarkTheme : LightTheme}>
         <AppContext.Provider
-          value={{ darkTheme, toggleTheme, hamburger, setHamburger }}
-        >
-          <div className="App">
+          value={{ darkTheme, toggleTheme, hamburger, setHamburger }}>
+          <div className='App'>
             <Navbar />
             <Toolbar />
-            <span id="meet"></span>
-            Hello
+            <span id='meet'></span>
           </div>
         </AppContext.Provider>
       </ThemeProvider>

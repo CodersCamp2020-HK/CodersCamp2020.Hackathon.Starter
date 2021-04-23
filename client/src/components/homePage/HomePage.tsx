4557165d2cd8828591dafac4d2c8ffb8d717aadb
Haivex <demozial@gmail.com>
2021-04-23 23:58:19 +0200

    Added text and two buttons from first page

---
+++ client/src/components/homePage/HomePage.tsx
@@ -0,0 +1,29 @@
+import { Button, makeStyles, TextField, Typography } from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles({
+  column: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const HomePage = () => {
+  const styles = useStyles();
+  return (
+    <div className={styles.column}>
+      <Typography variant="h5">Ile będzie trwało spotkanie?</Typography>
+      <TextField variant='contained' size="medium" />
+      <Button variant="outlined" size="medium">
+        Dodaj prezentacje
+      </Button>
+      <Button variant="contained" size="medium">
+        Utwórz spotkanie
+      </Button>
+    </div>
+  );
+};
+
+export default HomePage;

2ac62ec74b427e26cbdcddf61d1233ed0d4edd3b
Hubert Kawałek <hubert.kawalek.dev@gmail.com>
2021-04-24 12:45:00 +0200

    Meeting view

--- client/src/pages/Meeting.tsx
+++ client/src/pages/Meeting.tsx
@@ -4,26 +4,26 @@ import Actions from "../components/actions/Actions";
 import Timer from "../components/common/timer/Timer";
 import NotificationWrapper from "../components/notifications/NotificationWrapper";
 import Nav from "../components/nav/Nav";
-import { DemoEvents } from "../events/DemoEvents";
 import Chatbox, { SingleComment } from "../components/chatbox/Chatbox";
 import { useMeetingEvents } from "../events/Meeting";
 import EnterMeeting from "../components/enterMeeting/EnterMeeting";
+import { JitsiFrame } from "../components/jitsi/JitsiFrame";
 
 const comments: SingleComment[] = [
   {
-    name: 'Mateusz',
-    textMessage: 'Elo',
-    time: '12:52',
+    name: "Mateusz",
+    textMessage: "Elo",
+    time: "12:52",
   },
   {
-    name: 'Mateusz',
-    textMessage: 'EloELo',
-    time: '12:53',
+    name: "Mateusz",
+    textMessage: "EloELo",
+    time: "12:53",
   },
   {
-    name: 'Mateusz',
-    textMessage: 'EloEloElo',
-    time: '12:54',
+    name: "Mateusz",
+    textMessage: "EloEloElo",
+    time: "12:54",
   },
 ];
 
@@ -34,22 +34,21 @@ const Meeting = () => {
   return (
     <Switch>
       <Route exact path={`${path}/:name`}>
-        {!participant
-          ? 
-            <EnterMeeting />
-          :
+        {!participant ? (
+          <EnterMeeting />
+        ) : (
           <>
             <Nav />
-            <DemoEvents />
+            <JitsiFrame />
             <Timer timeInSeconds={120} />
-              Hello
+            Hello
             <Actions />
             <NotificationWrapper />
             <Chatbox comments={comments} />
           </>
-        }
+        )}
       </Route>
-      <Redirect to={`/404${path}`} />
+      <Redirect to={`/404/${path}`} />
     </Switch>
   );
 };

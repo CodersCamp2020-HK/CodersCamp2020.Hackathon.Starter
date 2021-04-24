0afd911d00af7fe01bc1c74474f2d949fc5bc5c5
Ania-Em <aniaarchi@gmail.com>
2021-04-24 05:26:56 +0200

    Theme colors

--- client/src/themes/DarkTheme.tsx
+++ client/src/themes/DarkTheme.tsx
@@ -3,10 +3,50 @@ import { purple } from "@material-ui/core/colors";
 
 const DarkTheme = responsiveFontSizes(
   createMuiTheme({
+    typography: {
+      allVariants: {
+        color: "#B2FEF7",
+      },
+    },
     palette: {
       type: "dark",
       primary: {
-        main: purple[500],
+        light: "#B2FEF7",
+        main: "#80CBC4",
+        dark: "#4F9A94",
+        contrastText: "#000000",
+      },
+      secondary: {
+        light: "#FFDDC1",
+        main: "#FFAB91",
+        dark: "#C97B63",
+        contrastText: "#000000",
+      },
+      common: {
+        white: "#202020",
+      },
+      background: {
+        default: "#363636",
+      },
+    },
+    overrides: {
+      MuiButton: {
+        root: {
+          borderRadius: 18,
+        },
+        sizeSmall: {
+          borderRadius: 15,
+        },
+        sizeLarge: {
+          borderRadius: 21,
+        },
+      },
+      MuiTextField: {
+        root: {
+          "& fieldset": {
+            borderRadius: 18,
+          },
+        },
       },
     },
   })
--- client/src/themes/LightTheme.tsx
+++ client/src/themes/LightTheme.tsx
@@ -3,16 +3,49 @@ import { green, lightBlue } from "@material-ui/core/colors";
 
 const LightTheme = responsiveFontSizes(
   createMuiTheme({
+    typography: {
+      allVariants: {
+        color: "#007769"
+      }},
     palette: {
       type: "light",
       primary: {
-        main: green[500],
+        light: "#48a697",
+        main: "#007769",
+        dark: "#004a3f",
+        contrastText: "#ffffff",
       },
       secondary: {
-        main: lightBlue[200],
+        light: "#ff7b47",
+        main: "#e54819",
+        dark: "#ab0300",
+        contrastText: "#ffffff",
       },
+      background: {
+        default: '#F1F1F1',
     },
-  })
+    },
+    overrides: {
+      MuiButton: {
+          root: {
+              borderRadius: 18,
+          },
+          sizeSmall: {
+              borderRadius: 15,
+          },
+          sizeLarge: {
+              borderRadius: 21,
+          },
+      },
+      MuiTextField: {
+          root: {
+              '& fieldset': {
+                  borderRadius: 18,
+              },
+          },
+      },
+  }})
 );
+
 
 export { LightTheme };

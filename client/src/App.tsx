a1ac6c9fe38b11edb68bb5e4c46ce3ecdeebda2c
Weronika <wera02@gmail.com>
2021-04-24 09:56:52 +0200

    Shorter options on particles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -70,136 +70,55 @@ function App() {
           <div className="App">
             <Navbar />
             <Toolbar />
-            <Particles
-      options={{
-        backgroundMode: {
-          enable: true,
-          zIndex: 0
-        },
-        particles: {
-          number: {
-            value: 200,
-            limit: 300,
-            density: {
-              enable: true,
-              value_area: 800
-            }
-          },
-          color: {
-            value: "#ffffff"
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 0,
-              color: "#000000"
-            },
-            polygon: {
-              nb_sides: 5
-            },
-          },
-          opacity: {
-            value: 0.5,
-            random: true,
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0.5,
-              sync: false
-            }
-          },
-          size: {
-            value: 30,
-            random: true,
-            anim: {
-              enable: true,
-              speed: 10,
-              size_min: 10,
-              sync: false
-            }
-          },
-          line_linked: {
-            enable: true,
-            distance: 100,
-            color: "#ffffff",
-            opacity: 1,
-            width: 1
-          },
-          move: {
-            enable: true,
-            speed: 3,
-            direction: "none",
-            random: false,
-            straight: false,
-            out_mode: "out",
-            bounce: false,
-            attract: {
-              enable: false,
-              rotateX: 600,
-              rotateY: 1200
-            }
-          }
-        },
+            <Particles className='particles' options={{
+        detectRetina: true,
+        fpsLimit: 60,
+            
         interactivity: {
-          detect_on: "canvas",
-          events: {
-            onHover: {
-              enable: true,
-              mode: "bubble",
-              parallax: {
-                enable: false,
-                force: 60,
-                smooth: 10
-              }
+            detectsOn: "canvas",
+            modes: {
+                bubble: {
+                    distance: 700,
+                    duration: 2,
+                    opacity: 0.4,
+                    size: 50,
+                },
             },
-            onClick: {
-              enable: true,
-              mode: "push"
-            },
-            resize: true
-          },
-          modes: {
-            grab: {
-              distance: 400,
-              lineLinked: {
-                opacity: 1
-              }
-            },
-            bubble: {
-              distance: 400,
-              size: 100,
-              duration: 2,
-              opacity: 1,
-            },
-            repulse: {
-              distance: 200
-            },
-            push: {
-              particles_nb: 4
-            },
-            remove: {
-              particles_nb: 2
-            }
-          }
         },
-        backgroundMask: {
-          enable: true,
-          cover: {
+
+        particles: {
             color: {
-              value: {
-                r: 0,
-                g: 0,
-                b: 0,
-              },
-            },
-            opacity: 1,
-          }
+            value: "#999999",
         },
-        retina_detect: true,
-        fps_limit: 60,
-      }
-    }
-    />
+
+        move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: true,
+            speed: 3,
+            straight: false,
+        },
+
+        number: {
+            density: {
+            enable: true,
+            value_area: 900,
+            },
+
+            value: 30,
+        },
+
+        shape: {
+            type: "circle",
+        },
+
+        size: {
+            random: true,
+            value: 3,
+        },
+    },
+    }}/>
           </div>
         </AppContext.Provider>
       </ThemeProvider>

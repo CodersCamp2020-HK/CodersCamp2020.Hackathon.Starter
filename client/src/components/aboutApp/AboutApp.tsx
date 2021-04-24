3fd9e326c685887ca5e2cd96c379dc33c79234ee
Haivex <demozial@gmail.com>
2021-04-24 11:37:25 +0200

    Added For user buttons info

--- client/src/components/aboutApp/AboutApp.tsx
+++ client/src/components/aboutApp/AboutApp.tsx
@@ -69,19 +69,53 @@ const AboutApp = () => {
                 <li>It makes meetings not a boring necessity, and becomes an opportunity for integration, exchange of ideas and quick problem solving</li>
             </ul>
         </Typography>
-        <Typography variant="h6">
+        <Typography variant="h5">
             Time control
         </Typography>
         <img className={styles.image} alt='placeholder' src='https://via.placeholder.com/468x60' />
         <Typography variant='body1'>
             Each user has a preview of the time allocated to the meeting, how much time is left to the end, it is visualized in the form of a moving slider.
         </Typography>
-        <Typography variant='h6'>
+        <Typography variant='h5'>
             Record of the meeting 
         </Typography>
         <Typography variant='body1'>
            Description
         </Typography>
+        <Typography variant='h5'>
+            For participant
+        </Typography>
+        <img className={styles.image} alt='placeholder' src='https://via.placeholder.com/468x60' />
+        <Typography variant='h6'>
+            Ask for a break
+        </Typography>
+        <Typography variant='body1'>
+        If the participant feels tired, they can click this button to request a break. The organizer can then accept the request and the meeting window changes to the corresponding image 
+        </Typography>
+        <Typography variant='h6'>
+            I don't understand :(
+        </Typography>
+        <Typography variant='body1'>
+        If the participant does not understand the issue, he can click this button to inform the organizer for a more detailed explanation.
+        </Typography>
+        <Typography variant='h6'>
+            Change the topic?
+        </Typography>
+        <Typography variant='body1'>
+        If the participant thinks that the topic has been properly explained, he can inform other participants that he wants to change the topic 
+        </Typography>
+        <Typography variant='h6'>
+            Make sense / I don't agree
+        </Typography>
+        <Typography variant='body1'>
+            During the brainstorming session, users can express their position (consent or against ) by clicking on a thumb button 
+        </Typography>
+
+
+        <Typography variant='h5'>
+            For meeting organizer
+        </Typography>
+        <img className={styles.image} alt='placeholder' src='https://via.placeholder.com/468x60' />
       </Container>
     </PerfectScrollbar>
   );

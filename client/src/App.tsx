ee332cad436d0f94522a5ccad546475809f755f2
Weronika <wera02@gmail.com>
2021-04-24 09:18:40 +0200

    [NW] commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import { RestfulProvider } from "restful-react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { DarkTheme } from "./themes/DarkTheme";
 import { LightTheme } from "./themes/LightTheme";
+import Particles from "react-tsparticles";
 
 const isProductionEnv = process.env.NODE_ENV === "production";
 const devApiUrl = "http://localhost:8000";
@@ -46,6 +47,7 @@ function App() {
   });
   const [hamburger, setHamburger] = useState(false);
 
+  
   const toggleTheme = () => {
     if (darkTheme) {
       localStorage.removeItem(StorageThemeKey);
@@ -68,8 +70,136 @@ function App() {
           <div className="App">
             <Navbar />
             <Toolbar />
-            <span id="meet"></span>
-            Hello
+            <Particles
+      options={{
+        backgroundMode: {
+          enable: true,
+          zIndex: 0
+        },
+        particles: {
+          number: {
+            value: 200,
+            limit: 300,
+            density: {
+              enable: true,
+              value_area: 800
+            }
+          },
+          color: {
+            value: "#ffffff"
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000"
+            },
+            polygon: {
+              nb_sides: 5
+            },
+          },
+          opacity: {
+            value: 0.5,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.5,
+              sync: false
+            }
+          },
+          size: {
+            value: 30,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 10,
+              size_min: 10,
+              sync: false
+            }
+          },
+          line_linked: {
+            enable: true,
+            distance: 100,
+            color: "#ffffff",
+            opacity: 1,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 3,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+            }
+          }
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onHover: {
+              enable: true,
+              mode: "bubble",
+              parallax: {
+                enable: false,
+                force: 60,
+                smooth: 10
+              }
+            },
+            onClick: {
+              enable: true,
+              mode: "push"
+            },
+            resize: true
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              lineLinked: {
+                opacity: 1
+              }
+            },
+            bubble: {
+              distance: 400,
+              size: 100,
+              duration: 2,
+              opacity: 1,
+            },
+            repulse: {
+              distance: 200
+            },
+            push: {
+              particles_nb: 4
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
+        },
+        backgroundMask: {
+          enable: true,
+          cover: {
+            color: {
+              value: {
+                r: 0,
+                g: 0,
+                b: 0,
+              },
+            },
+            opacity: 1,
+          }
+        },
+        retina_detect: true,
+        fps_limit: 60,
+      }
+    }
+    />
           </div>
         </AppContext.Provider>
       </ThemeProvider>
